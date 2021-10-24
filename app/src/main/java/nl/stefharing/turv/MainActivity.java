6ce0bf979d152bc82177d1d500ece7e6a75af524
Stef Haring <s1125097@hsleiden.nl>
2021-10-24 15:09:36 +0200

    First commit

---
+++ app/src/main/java/nl/stefharing/turv/MainActivity.java
@@ -0,0 +1,15 @@
+package nl.stefharing.turv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //test
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
